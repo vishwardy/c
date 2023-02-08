ff9aa51a5acfad173674e8c2d6c00e3f5fb0a6ac
vishwadeepreddy <105554822+vishwardy@users.noreply.github.com>
2023-02-08 21:26:29 +0530

    Create pointers_basic.c

---
+++ pointers_basic.c
@@ -0,0 +1,9 @@
+//pointers
+#include<stdio.h>
+int main()
+{
+	int n=555;
+	printf("\n value of n=%d",n);    //value of n
+	printf("\n address of n=%u",&n); //unsignedint
+	printf("\n address of n=%p",&n); //hexadecimal
+}
